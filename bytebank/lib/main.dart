050a4639ed64dce2210d1fdf7f2848f99093ab52
Florenncio <36462896+Florenncio@users.noreply.github.com>
2020-09-28 00:12:30 -0300

    #2.1

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,26 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    body: ListaTransferencias(),
-    appBar: AppBar(
-      title: Text('Transferências'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Icon(Icons.add),
-    ),
-  ),
-));
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          body: FormularioTransferencias(),
+        )
+    );
+  }
+}
+
+
+class FormularioTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Criando Transferencias')),
+      body: Text('Florencio')
+    );
+  }
+}
+
 
 class ListaTransferencias extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ItemTransferencias(Transferencia(123.0, 1000)),
-        ItemTransferencias(Transferencia(200.0, 1001)),
-        ItemTransferencias(Transferencia(330.2, 2002)),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: Column(
+        children: <Widget>[
+          ItemTransferencias(Transferencia(123.0, 1000)),
+          ItemTransferencias(Transferencia(200.0, 1001)),
+          ItemTransferencias(Transferencia(330.2, 2002)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
